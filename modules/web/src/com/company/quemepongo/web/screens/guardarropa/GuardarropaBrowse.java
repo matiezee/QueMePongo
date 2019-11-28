1f231d6dc61e860777e239d943c21f18cd0b56e1
Sofia Enriquez <lsofia.enriquez@gmail.com>
2019-11-27 21:02:59 -0300

    Logica para sugerencias terminada.

--- modules/web/src/com/company/quemepongo/web/screens/guardarropa/GuardarropaBrowse.java
+++ modules/web/src/com/company/quemepongo/web/screens/guardarropa/GuardarropaBrowse.java
@@ -1,19 +1,21 @@
 package com.company.quemepongo.web.screens.guardarropa;
 
+import com.company.quemepongo.entity.Guardarropa;
 import com.company.quemepongo.entity.Prenda;
 import com.company.quemepongo.entity.Sugerencia;
-import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.gui.components.SuggestionField;
 import com.haulmont.cuba.gui.components.Table;
 import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.model.CollectionPropertyContainer;
-import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
-import com.company.quemepongo.entity.Guardarropa;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Random;
+
 
 @UiController("quemepongo_Guardarropa.browse")
 @UiDescriptor("guardarropa-browse.xml")
@@ -25,29 +27,27 @@ public class GuardarropaBrowse extends MasterDetailScreen<Guardarropa> {
     @Inject
     private CollectionContainer<Guardarropa> guardarropasDc;
     @Inject
-    private CollectionPropertyContainer<Sugerencia> sugerenciasDc;
+    private DataManager dataManager;
     @Inject
-    private Metadata metadata;
+    private Table<Sugerencia> sugerenciasTable;
+    @Inject
+    private SuggestionField suggestionField;
 
     public void onGenerarSugerenciaButtonClick() {
 
+        // Obtengo el guardarropa
         Guardarropa g1 = guardarropasDc.getItems().get(0);
-        Sugerencia s1 = metadata.create(Sugerencia.class);
-
-        s1.setGuardarropa(g1);
 
         List<Prenda> torsos = new ArrayList<>();
         List<Prenda> piernas = new ArrayList<>();
         List<Prenda> calzados = new ArrayList<>();
         List<Prenda> accesorios = new ArrayList<>();
-
         List<Prenda> lista_prendas = new ArrayList<>();
 
         ListIterator<Prenda> prendas = prendasDc.getMutableItems().listIterator();
 
         // Reemplazar por un foreach para mejorar la performance
         while (prendas.hasNext()) {
-
             Prenda p_aux = prendas.next();
             switch (p_aux.getTipoDePrenda().getCategoria().getNombre()) {
                 case "Torso":
@@ -67,52 +67,51 @@ public class GuardarropaBrowse extends MasterDetailScreen<Guardarropa> {
             }
         }
 
-        /*
+        lista_prendas.add(this.obtenerPrendaRandom(torsos));
+        lista_prendas.add(this.obtenerPrendaRandom(piernas));
+        lista_prendas.add(this.obtenerPrendaRandom(calzados));
+        if (this.randomBoolean() == true) {
+            lista_prendas.add(this.obtenerPrendaRandom(accesorios));
+        }
 
-        Logica del producto vectorial con Guava
-        Falta ver como mostrarlo primero
+        // Crear la instancia
+        Sugerencia s1 = dataManager.create(Sugerencia.class);
 
-        String[] torsoArray = new String[ torsos.size() ];
-        String[] piernasArray = new String[ torsos.size() ];
-        String[] accesoriosArray = new String[ torsos.size() ];
-        String[] calzadoArray = new String[ torsos.size() ];
+        try {
 
-        torsos.toArray( torsoArray );
-        piernas.toArray( piernasArray );
-        calzados.toArray( calzadoArray );
-        accesorios.toArray( accesoriosArray );
+            // Creo una instancia de Sugerencias
+            s1.setGuardarropa(g1);
+            s1.setPrenda(lista_prendas);
 
-        System.out.println("DESPUES");
-        List<String[]> elements = Arrays.asList(
-                torsoArray,
-                piernasArray,
-                accesoriosArray,
-                calzadoArray
-        );
+            // Guardo en la base de datos la Sugerencia
+            dataManager.commit(s1);
 
-        // Create a list of immutableLists of strings
-        List<ImmutableList<String>> immutableElements = makeListofImmutable(elements);
+            //Muestro un mensaje de exito
 
-        // Use Guava's Lists.cartesianProduct, since Guava 19
-        List<List<String>> cartesianProduct = Lists.cartesianProduct(immutableElements);
-        */
+            suggestionField.setValue("Sugerencia realizada con exito, por favor refrescar la pagina. " +
+                    "Sugerencia ID: " + s1.getId());
+            suggestionField.setContextHelpText("Atencion");
 
+        } catch (Exception e) {
+            // Borramos el objeto by id ya que no se guardo bien o algo paso.
+            dataManager.remove(dataManager.getReference(Sugerencia.class, s1.getId()));
+            System.out.println("Borrando el objeto mal creado: " + e);
+        }
 
-        lista_prendas.add(torsos.get(0));
-        lista_prendas.add(piernas.get(0));
-        lista_prendas.add(calzados.get(0));
-        lista_prendas.add(accesorios.get(0));
-
-        s1.setPrenda(lista_prendas);
-        System.out.println("Sugerencia creada exitosamente. Sugerencia id " + s1.getId() + " dentro del guardarropas: " + s1.getGuardarropa() + " Con las prendas: " + s1.getPrenda().toString());
-
-        /*
-        No puedo lograr la nueva sugerencia en sugerenciasDc y creo que por eso no se muestra
-
-        sugerenciasDc.setItem(s1);
-        sugerenciasDc.replaceItem(s1);
-         */
-
+        // Refrescamos la tabla para que figure la nueva sugerencia
+        sugerenciasTable.refresh();
     }
+
+
+    public static Prenda obtenerPrendaRandom(List<Prenda> array) {
+        int rnd = new Random().nextInt(array.size());
+        return array.get(rnd);
+    }
+
+    public boolean randomBoolean() {
+        return Math.random() < 0.5;
+    }
+
+
 
 }
